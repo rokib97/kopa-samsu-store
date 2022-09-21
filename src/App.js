6e3afccb47a79194b5e379c555ab19d70cd3e6e8
rokib97 <hasanrokib1997@gmail.com>
2022-09-22 00:48:43 +0600

    added new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import AllGun from "./Components/AllGun/AllGun";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const countIncrease = () => {
+    setCount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar count={count} />
+      <AllGun countIncrease={countIncrease} />
     </div>
   );
 }
-
 export default App;
